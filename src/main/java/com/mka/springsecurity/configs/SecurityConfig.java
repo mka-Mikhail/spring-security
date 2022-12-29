70b2832fdd4ef0c58659bc78e79c92ae49c6ecf3
mka-Mikhail <mixailrojnow@yandex.ru>
2022-12-29 23:45:51 +0300

    бин для UserDetails

--- src/main/java/com/mka/springsecurity/configs/SecurityConfig.java
+++ src/main/java/com/mka/springsecurity/configs/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.mka.springsecurity.configs;
 
-import ch.qos.logback.core.encoder.EchoEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,5 +23,16 @@ public class SecurityConfig {
                 .requestMatchers("/api/**").authenticated()
                 .and()
                 .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username("user")
+                .password("pass")
+                .authorities("admin", "super admin")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
     }
 }
